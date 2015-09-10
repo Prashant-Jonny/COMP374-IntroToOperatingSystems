15b259967ddc6141ed898f9bde077e2ecdab15ae
chernandez7 <chernandez7@luc.edu>
2015-09-10 13:56:30 -0500

    Hw2 a bit more work done, actually creates txt file now

--- Homework2/Homework2/Homework2.cpp
+++ Homework2/Homework2/Homework2.cpp
@@ -23,15 +23,20 @@ int _tmain(int argc, char *argv[])
 {
 	clock_t start, end;
 	double time, rate;
-	int byteAmount = 1024;
-	long bufferSize;
-
-	bufferSize = strtol(argv[1], NULL, 10);
+	int byteAmount = 1024; //Need to figure out how to determine amount of bytes transferred.
+	long bufferSize = strtol(argv[1], NULL, 10); //Need to figure out how to transfer by specific amount of bytes at a time
 	ifstream sourceFile;
 	ofstream  newFile;
-	sourceFile.open( argv[2] );
-	newFile.open(argv[3], ios::out);
 
+	char fileName[30];
+	strcpy(fileName, argv[3]);
+	strcat(fileName, ".txt");
+
+	//Open source and create new file to copy to.
+	sourceFile.open( argv[2] ); //Is only taking in first char as input
+	newFile.open(fileName, ios::out);
+
+	//If too many arguments are given end execution
 	if (argc > 4)
 	{
 		cerr << "Too many parameters given." << endl;
@@ -39,33 +44,39 @@ int _tmain(int argc, char *argv[])
 		return 1;
 	}
 
+	//If everything opens ok then begin timing and copying
 	if ( sourceFile.is_open() && newFile.is_open() )
 	{
 		start = clock();
 
+		//Needs modification to do it by variable buffer sizes
 		sourceFile.seekg(0, ios::end);
-		ifstream::pos_type s = sourceFile.tellg();
+		//ifstream::pos_type s = sourceFile.tellg();
+		ifstream::pos_type s = bufferSize;
 		sourceFile.seekg(0);
 		char* buffer = new char[s];
 
-		sourceFile.read(buffer, s);
-		newFile.write(buffer, s);
-		
+			sourceFile.read(buffer, s);
+			newFile.write(buffer, s);		
+
+		//Cleanup of memory
 		end = clock();
 		delete[] buffer;
 		sourceFile.close();
 		newFile.close();
 	}
+	//If files don't open up then give error and close program.
 	else
 	{
 		cerr << "Chosen source file " << argv[2] << " can't be open or doesn't exist." << endl;
 		return 1;
 	}
 
-
-	time = ( end - start ) / CLOCKS_PER_SEC;
+	//Calculate time spent and print result.
+	//byteAmount = counter * bufferSize;
+	time = end - start ;
 	rate = byteAmount / time;
-	cout << "Copied " << byteAmount<< " bytes in " << time << " seconds at the rate of " << rate << " bytes per second." << endl;
+	cout << "Copied " << byteAmount<< " bytes in " << time << " seconds at the rate of " << rate << " bytes per millisecond." << endl;
 
 
 	return 0;
